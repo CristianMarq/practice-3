cf1d5df3e864a459bd3ad7b80d795195ecc26ee2
Christian Marquez <marquezcristian920@gmail.com>
2021-07-14 21:12:11 -0600

    Terminacion de la logica de programacion

--- src/js/logica.js
+++ src/js/logica.js
@@ -1,6 +1,29 @@
 import { Producto } from "./Producto";
 
+const tbody = document.querySelector("#productos-desc tbody");
 let productos = [];
+
+const creaRow = () => {
+  let lista = productos;
+  tbody.innerHTML = "";
+
+  for (let i = 0; i < lista.length; i++) {
+    let row = tbody.insertRow(i);
+    let codCell = row.insertCell(0),
+      nombreCell = row.insertCell(1),
+      descripcionCell = row.insertCell(2),
+      precioCell = row.insertCell(3),
+      existenciasCell = row.insertCell(4);
+
+    codCell.innerHTML = lista[i].codigoProducto;
+    nombreCell.innerHTML = lista[i].nombre;
+    descripcionCell.innerHTML = lista[i].descripcion;
+    precioCell.innerHTML = `$${lista[i].precioVenta}`;
+    existenciasCell.innerHTML = lista[i].existencias;
+
+    tbody.appendChild(row);
+  }
+};
 
 export const creaProducto = (
   codigoProducto,
@@ -17,5 +40,5 @@ export const creaProducto = (
     existencias
   );
   productos.push(producto);
-  console.log(productos);
+  creaRow();
 };
